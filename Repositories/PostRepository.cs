1b4792a3c03314d1726179a082d5d36dc5ea980c
17-33441-1@student.aiub.edu <17-33441-1@student.aiub.edu>
2020-12-21 00:15:01 +0600

    Add project files.

---
+++ Repositories/PostRepository.cs
@@ -0,0 +1,19 @@
+﻿using SocialHub.Models;
+//using Inventory_with_Repository_Pattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace SocialHub.Repositories
+{
+    public class ProductRepository:Repository<Post>
+    {
+        /*public List<Post> GetTopProducts(int top)
+        {
+            return this.GetAll().OrderByDescending(x => x.Price).Take(top).ToList();
+        }
+        */
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using SocialHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalAssignment.Repositories
+{
+    public class UserRepository:Repository<User>
+    {
+
+    }
+}
